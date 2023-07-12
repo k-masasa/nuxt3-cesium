6ea9d17c29cfcaf22ad42c5b29b148957dc27d91
Kumagai <masafumi-kumagai-s@sensyn-robotics.com>
2023-07-12 15:39:24 +0900

    setting vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  // devtools: { enabled: true }
+  build: {
+    transpile: ['vuetify'],
+  },
+  hooks: {
+    // ビルド時にVuetifyを読み込む
+    'vite:extendConfig': (config) => {
+      config.plugins!.push(vuetify())
+    },
+  },
+  vite: {
+    ssr: {
+      // SSR時にVuetifyを依存関係として読み込まない
+      noExternal: ['vuetify'],
+    },
+    // グローバル定数を定義
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  css: ['@/assets/main.scss'],
 })
