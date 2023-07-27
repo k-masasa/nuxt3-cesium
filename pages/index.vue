37ad68c112e23d86f91764c5d3e308fb5a4c112f
Kumagai <masafumi-kumagai-s@sensyn-robotics.com>
2023-07-27 23:17:47 +0900

    draft

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
+  <!-- <vc-config-provider :cesium-path="vcConfig.cesiumPath">
+    <vc-viewer></vc-viewer>
+  </vc-config-provider> -->
   test
 </template>
 
 <script setup lang="ts">
+// import { reactive } from 'vue'
+// import { VcConfigProvider, VcViewer } from 'vue-cesium'
+
+// const vcConfig = reactive({
+//   cesiumPath: 'https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js'
+// })
 </script>
--- plugins/cesium-vue.ts
+++ plugins/cesium-vue.ts
@@ -2,7 +2,8 @@ import VueCesium from 'vue-cesium'
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.use(VueCesium, {
-    cesiumPath: 'https://unpkg.com/cesium/Build/Cesium/Cesium.js',
+    // cesiumPath: 'https://unpkg.com/cesium/Build/Cesium/Cesium.js',
+    cesiumPath: '',
     accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNDA4YjUyMS1hOTc1LTRjMzMtYTU4YS1hZmJjMDJkZGNlZjYiLCJpZCI6MTU2ODU5LCJpYXQiOjE2OTA0NjYwODl9.3NtpmgVFU_XiRbQWj-KWRZ6azfM6g_nWGsmi9xJIfxk'
   })
 })
