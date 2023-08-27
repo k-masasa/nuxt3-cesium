2c233df0add07cd97dba80e78f13f1ba67fe8551
Kumagai <masafumi-kumagai-s@sensyn-robotics.com>
2023-08-27 18:33:07 +0900

    draft

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
-  <!-- <vc-config-provider :cesium-path="vcConfig.cesiumPath">
+  <vc-config-provider :cesium-path="vcConfig.cesiumPath">
     <vc-viewer></vc-viewer>
-  </vc-config-provider> -->
+  </vc-config-provider>
   test
 </template>
 
 <script setup lang="ts">
-// import { reactive } from 'vue'
-// import { VcConfigProvider, VcViewer } from 'vue-cesium'
+import { reactive } from 'vue'
+import { VcConfigProvider, VcViewer } from 'vue-cesium'
 
-// const vcConfig = reactive({
-//   cesiumPath: 'https://unpkg.com/cesium@latest/Build/Cesium/Cesium.js'
-// })
+// window.CESIUM_BASE_URL = '/'
+const cesiumAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNDA4YjUyMS1hOTc1LTRjMzMtYTU4YS1hZmJjMDJkZGNlZjYiLCJpZCI6MTU2ODU5LCJpYXQiOjE2OTA0NjYwODl9.3NtpmgVFU_XiRbQWj-KWRZ6azfM6g_nWGsmi9xJIfxk'
+const vcConfig = reactive({
+  cesiumPath: './node_modules/cesium/Build/Cesium/Cesium.js',
+  accessToken: cesiumAccessToken,
+})
 </script>
--- plugins/cesium-vue.ts
+++ plugins/cesium-vue.ts
@@ -1,9 +1,10 @@
 import VueCesium from 'vue-cesium'
 
 export default defineNuxtPlugin(nuxtApp => {
+  window.CESIUM_BASE_URL = '/cesium'
+  const cesiumAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNDA4YjUyMS1hOTc1LTRjMzMtYTU4YS1hZmJjMDJkZGNlZjYiLCJpZCI6MTU2ODU5LCJpYXQiOjE2OTA0NjYwODl9.3NtpmgVFU_XiRbQWj-KWRZ6azfM6g_nWGsmi9xJIfxk'
   nuxtApp.vueApp.use(VueCesium, {
-    // cesiumPath: 'https://unpkg.com/cesium/Build/Cesium/Cesium.js',
-    cesiumPath: '',
-    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNDA4YjUyMS1hOTc1LTRjMzMtYTU4YS1hZmJjMDJkZGNlZjYiLCJpZCI6MTU2ODU5LCJpYXQiOjE2OTA0NjYwODl9.3NtpmgVFU_XiRbQWj-KWRZ6azfM6g_nWGsmi9xJIfxk'
+    cesiumPath: './node_modules/cesium/Build/Cesium/Cesium.js',
+    accessToken: cesiumAccessToken,
   })
 })
